c875256f517e64a6ac941606f2629f8699a69d32
Aleksey-Dr <aprel181818@gmail.com>
2023-05-31 19:39:14 +0300

    Refactoring <ContactForm>
    II. Basic initial settings #2
    1. Set Settings > Pages:
       Branch: gh-pages from folder "root"
    2. Code > About > ⚙ > Website > ☑ Use your GitHub Pages website
       (https://aleksey-dr.github.io/goit-react-hw-04-phonebook/).
    
    III. Adding and configuring tools
    1. npm install.
    2. npm i prop-types.
    3. npm i clsx.
    4. npm i shortid.
    
    IV. Copied all components from goit-react-hw-03-phonebook
    
    V. Refactoring <ContactForm>

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,93 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import shortid from 'shortid';
+
+import contacts from '../data/contacts';
+
+import ContactForm from './contactForm';
+import Filter from './filter';
+import ContactList from './contactList';
+
+export class App extends Component {
+  state = {
+    contacts: contacts,
+    filter: '',
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    parsedContacts &&
+      this.setState({
+        contacts: parsedContacts,
+      });
+  };
+
+  componentDidUpdate(_, prevState) {
+    this.state.contacts !== prevState.contacts &&
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts));
+  };
+
+  addContact = (name, number) => {
+    const contact = {
+      id: shortid.generate(),
+      name,
+      number,
+    };
+
+    let includesName = false;
+    this.state.contacts.map(contact => {
+      contact.name === name && (includesName = true);
+      return includesName;
+    });
+
+    includesName
+      ? alert(name + ' is already in contacts')
+      : this.setState(({ contacts }) => ({
+          contacts: [contact, ...contacts],
+        }));
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  changeFilter = event => {
+    this.setState({
+      filter: event.currentTarget.value,
+    });
+  };
+
+  render() {
+    const { filter } = this.state;
+
+    const normalizedFilter = this.state.filter.toLowerCase();
+    const filterContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+
+    return (
+      <div
+        style={{
+          boxSizing: 'border-box',
+          width: '420px',
+          margin: '20px',
+        }}
+      >
+        <h2>Phonebook</h2>
+
+        <ContactForm onSubmit={this.addContact} />
+
+        <h3>Contacts</h3>
+        <Filter value={filter} onChange={this.changeFilter} />
+
+        <ContactList
+          onDeleteContact={this.deleteContact}
+          filterContacts={filterContacts}
+        />
+      </div>
+    );
+  }
+}
